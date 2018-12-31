6f30f67d637456e7f02f26a6a160a25c5b6ec3e5
Thiago Ventura <thiventura@gmail.com>
2018-12-31 12:12:18 -0300

    Portrait mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show rootBundle;
+import 'package:flutter/services.dart';
 import 'package:camera/camera.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:tflite/tflite.dart';
@@ -22,13 +22,17 @@ Future<void> main() async {
   } on CameraException catch (e) {
     print('Error: ${e.code}\nError Message: ${e.description}');
   }
-  runApp(
-    MaterialApp(
-      title: 'Pokedex',
-      debugShowCheckedModeBanner: false,
-      home: Pokedex(),
-    )
-  );
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+    .then((_) {
+      runApp(
+        MaterialApp(
+          title: 'Pokedex',
+          debugShowCheckedModeBanner: false,
+          home: Pokedex(),
+        )
+      );
+    });
 }
 
 void loadPokemonList() {
